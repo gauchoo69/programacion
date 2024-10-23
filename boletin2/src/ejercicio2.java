f104760c07a87f2d20a375d2e37d152844c10463
gauchoo69 <gmarcelinodocio@danielcastelao.org>
2024-10-23 14:17:40 +0200

    hola soy putin

---
+++ boletin2/src/ejercicio2.java
@@ -0,0 +1,8 @@
+public class ejercicio2 {
+    public static void main(String[] args) {
+        double lado = 3;
+        double operacion = (lado*lado);
+
+        System.out.println("El area del cuadrado es:" + operacion);
+    }
+}
---
+++ ejercicio_multiplos/src/ejercicio_multiplos.java
@@ -0,0 +1,3 @@
+public class ejercicio_multiplos {
+    public static void main(String)
+}
