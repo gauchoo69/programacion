8873a81cc5fa7f14a89ef59ce73a4acb6c4be253
gauchoo69 <gmarcelinodocio@danielcastelao.org>
2024-11-18 14:08:17 +0100

    ej1

---
+++ boletin3_programacion/src/ejercicio1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduce un número: ");
+        double numero = sc.nextDouble();
+
+        if (numero > 0) {
+            System.out.println("É un número positivo.");
+        }
+        sc.close();
+    }
+}
+
