8cd7438d7936b6d86dee9300bbf1edb4432d42ab
gauchoo69 <gmarcelinodocio@danielcastelao.org>
2024-11-18 13:42:52 +0100

    hola este es mi boletin

---
+++ boletin2_programacion/src/ejercicio1.java
@@ -0,0 +1,12 @@
+public class ejercicio1 {
+
+    public static void main(String[] args) {
+        double base = 4;
+        double altura = 3;
+
+        double area = (base * altura) / 2;
+
+        System.out.println("el area del triangulo es: " +area);
+    }
+}
+
